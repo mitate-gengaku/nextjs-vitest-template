d1b807f09ab3cc5754216d0b7868be29ec05c994
mitate-gengaku <mitategengaku@gmail.com>
2025-03-05 21:55:09 +0900

    first commit

---
+++ app/vitest.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from 'vitest/config'
+import tsconfigPaths from "vite-tsconfig-paths"
+import react from '@vitejs/plugin-react';
+import path from 'path'
+
+export default defineConfig({
+  plugins: [react(),tsconfigPaths()],
+  resolve: {
+    alias: {
+      '@/src/': path.resolve(__dirname),
+    }
+  },
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    setupFiles: "./src/tests/setup/setup.ts"
+  },
+})
